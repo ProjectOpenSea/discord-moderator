41b739f5be222c458a793471f85e5ed332f0b3f1
Joshua Wu <wujoshuawu@gmail.com>
2021-02-26 22:58:46 +0900

    !message and !manage-messages

--- bot.py
+++ bot.py
@@ -1,8 +1,10 @@
 import argparse
 import asyncio
+from asyncio.tasks import Task
 import os
 import shlex
-from typing import Callable, Dict, cast
+from datetime import timedelta
+from typing import Dict, NoReturn, cast
 
 import discord
 from discord.abc import Messageable
@@ -15,7 +17,7 @@ CONFIRMATION = 'iknowwhatiamdoing'
 
 client = discord.Client()
 
-auto_messages: Dict[str, Callable[..., None]] = {}
+automessages: Dict[str, Task[NoReturn]] = {}
 
 
 def is_admin(member: Member) -> bool:
@@ -46,7 +48,7 @@ async def on_message(message: Message) -> None:
 
     try:
         if command == '!ban-all':
-            parser = argparse.ArgumentParser(description='Ban all users matching a given name.')
+            parser = argparse.ArgumentParser(prog="!ban-all", description='Ban all users matching a given name.')
             parser.add_argument('name', help='the name to match')
             parser.add_argument(f"--{CONFIRMATION}", action="store_true", help='actually execute the bans')
             args = vars(parser.parse_args(arguments))
@@ -60,19 +62,49 @@ async def on_message(message: Message) -> None:
             else:
                 await message.channel.send(f'Would ban {len(matched_members)} user(s).')
 
-        if command == '!automessage':
-            parser = argparse.ArgumentParser(description='Automatically post messages.')
+        if command == '!message':
+            parser = argparse.ArgumentParser(prog="!message", description='Automatically post messages.')
             parser.add_argument('message', nargs="+", help='the content of the message')
             parser.add_argument('--channel', required=True, help='the channel into which the message will be posted')
+            parser.add_argument('--seconds', type=int, default=0, help='period in seconds')
+            parser.add_argument('--minutes', type=int, default=0, help='period in minutes')
+            parser.add_argument('--hours', type=int, default=0, help='period in hours')
+            parser.add_argument('--days', type=int, default=0, help='period in days')
             args = vars(parser.parse_args(arguments))
             channel_name: str = args["channel"]
             content: str = " ".join(args["message"])
+            duration = timedelta(seconds=args["seconds"], minutes=args["minutes"], hours=args["hours"], days=args["days"]).total_seconds()
             for channel in guild.channels:
                 if channel.name == channel_name:
                     await cast(Messageable, channel).send(content)
+                    if duration > 0:
+                        async def run():
+                            while True:
+                                await asyncio.sleep(duration)
+                                await cast(Messageable, channel).send(content)
+                        loop = asyncio.get_event_loop()
+                        task = loop.create_task(run())
+                        automessages[content] = task
                     return
             await message.channel.send(f'No such channel found: {channel_name}')
 
+        if command == '!manage-messages':
+            parser = argparse.ArgumentParser(prog="!manage-messages", description='Manage automatic messages.')
+            parser.add_argument('--delete', type=int, help='the content of the message')
+            args = vars(parser.parse_args(arguments))
+            delete_id = args["delete"]
+            if delete_id is not None:
+                automessages.pop(list(automessages)[delete_id]).cancel()
+            else:
+                for i, msg in enumerate(automessages):
+                    await message.channel.send(f"{i}: {msg}")
+
+        else:
+            await message.channel.send(f'Unrecognized command: {command}')
+
+    except Exception as error:
+        print(error)
+        await message.channel.send(f'Error: {error}')
     except:
         pass
 
