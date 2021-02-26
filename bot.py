c673af069b9e2634f36602fc66c09c121de2c31a
Joshua Wu <wujoshuawu@gmail.com>
2021-02-26 20:54:03 +0900

    More typing

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+from typing import Callable, Dict, List, Optional, Tuple
 
 import discord
 from discord.member import Member
@@ -11,34 +12,40 @@ CONFIRMATION = 'iknowwhatiamdoing'
 
 client = discord.Client()
 
+auto_messages: Dict[str, Callable[..., None]] = {}
 
-def is_admin(member: Member):
+
+def is_admin(member: Member) -> bool:
     return any(role.permissions.administrator for role in member.roles)
 
 
-def parse(message: Message):
+def parse(message: Message) -> Tuple[str, Optional[List[str]]]:
     words = message.content.strip().split()
     return words[0], words[1:]
 
 
 @client.event
-async def on_ready():
+async def on_ready() -> None:
     print(f'We have logged in as {client.user}.')
 
 
 @client.event
-async def on_message(message: Message):
+async def on_message(message: Message) -> None:
     guild = message.guild
     member = message.author
+
     if (
         not guild
         or not isinstance(member, Member)
         or not is_admin(member)
         or not message.content
+        or not message.content.startswith("!")
         or member == client.user
     ):
         return
+
     command, arguments = parse(message)
+
     if command == '!ban-all':
         if not arguments:
             return
@@ -49,5 +56,6 @@ async def on_message(message: Message):
             await message.channel.send(f'Banned {len(matched_members)} user(s).')
         else:
             await message.channel.send(f'Would ban {len(matched_members)} user(s).')
+
 
 client.run(BOT_TOKEN)
