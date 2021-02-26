8a71f41319b051ce44cc324a65f081281e3732f7
Joshua Wu <wujoshuawu@gmail.com>
2021-02-26 21:30:34 +0900

    Use ArgumentParser

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
+import argparse
 import asyncio
 import os
-from typing import Callable, Dict, List, Optional, Tuple
+from typing import Callable, Dict
 
 import discord
 from discord.member import Member
@@ -19,11 +20,6 @@ def is_admin(member: Member) -> bool:
     return any(role.permissions.administrator for role in member.roles)
 
 
-def parse(message: Message) -> Tuple[str, Optional[List[str]]]:
-    words = message.content.strip().split()
-    return words[0], words[1:]
-
-
 @client.event
 async def on_ready() -> None:
     print(f'We have logged in as {client.user}.')
@@ -39,23 +35,31 @@ async def on_message(message: Message) -> None:
         or not isinstance(member, Member)
         or not is_admin(member)
         or not message.content
-        or not message.content.startswith("!")
+        or not message.content.startswith('!')
         or member == client.user
     ):
         return
 
-    command, arguments = parse(message)
+    [command, *arguments] = message.content.strip().split()
 
-    if command == '!ban-all':
-        if not arguments:
-            return
-        name_to_match = arguments[0]
-        matched_members = [m for m in guild.members if m.name == name_to_match]
-        if len(arguments) == 2 and arguments[1] == CONFIRMATION:
-            await asyncio.gather(*[guild.ban(m, delete_message_days=7) for m in matched_members])
-            await message.channel.send(f'Banned {len(matched_members)} user(s).')
-        else:
-            await message.channel.send(f'Would ban {len(matched_members)} user(s).')
+    try:
+        if command == '!ban-all':
+            parser = argparse.ArgumentParser(description='Ban all users matching a given name.')
+            parser.add_argument('name', help='the name to match')
+            parser.add_argument(f"--{CONFIRMATION}", action="store_true", help='actually execute the bans')
+            args = parser.parse_args(arguments)
+            matched_members = [m for m in guild.members if m.name == args.name]
+            if args[CONFIRMATION]:
+                await message.channel.send(f'Banning {len(matched_members)} user(s)...')
+                await asyncio.gather(*[guild.ban(m, delete_message_days=7) for m in matched_members])
+                await message.channel.send(f'Banned {len(matched_members)} user(s).')
+            else:
+                await message.channel.send(f'Would ban {len(matched_members)} user(s).')
+
+        if command == '!auto-message':
+            pass
+    except:
+        pass
 
 
 client.run(BOT_TOKEN)
